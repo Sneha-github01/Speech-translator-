5a32446c036933f915d38baa5f4f5d0cf487aa48
Sneha satapathy <141478220+Sneha-github01@users.noreply.github.com>
2024-01-18 19:54:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import os
-
 import speech_recognition as sr
-from google_trans_new import google_translator
+from googletrans import Translator
 from gtts import gTTS
 from playsound import playsound
 
 r = sr.Recognizer()
-translator = google_translator()
+translator = Translator()
 
 while True:
- with sr.Microphone() as source:
-     print("Speak now!")
-     audio = r.listen(source)
-     try:
-         speech_text = r.recognize_google(audio)
-         print(speech_text)
-         if(speech_text =="exit"):
-             break
+    with sr.Microphone() as source:
+        print("Speak now!")
+        audio = r.listen(source)
+        try:
+            speech_text = r.recognize_google(audio)
+            print(speech_text)
+            if speech_text.lower() == "exit":
+                break
 
-     except sr.UnknownValueError:
-         print("Couldn't understand")
-     except sr.RequestError:
-         print("Couldn't request results from google")
+            try:
+                translated_text = translator.translate(speech_text, dest='fr').text
+                print(translated_text)
 
-     translated_text = translator.translate(speech_text, lang_tgt='fr')
-     print(translated_text)
+                voice = gTTS(translated_text, lang='fr')
+                voice.save("voice.mp3")
+                playsound("voice.mp3")
+                os.remove("voice.mp3")
 
-     voice = gTTS(translated_text, lang='fr')
-     voice.save("voice.mp3")
-     playsound("voice.mp3")
-     os.remove("voice.mp3")
+            except Exception as translation_error:
+                print(f"Translation error: {str(translation_error)}")
 
-
-
+        except sr.UnknownValueError:
+            print("Couldn't understand")
+        except sr.RequestError as request_error:
+            print(f"Couldn't request results from Google: {str(request_error)}")
